1601972a392fe85ad13ea4dffd3265669e677a21
Esteban Vasco <evasco.27@hotmail.com>
2022-10-03 15:47:03 -0500

    Creando el componente bebidas
    Se crea el componente bebidas con su logica

--- src/components/Bebidas.jsx
+++ src/components/Bebidas.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { useContext } from "react";
+import { Card, Button, Col } from "react-bootstrap";
+import { BebidasContext } from "../context/BebidasProvider";
 
 const Bebidas = () => {
+  const { bebidas } = useContext(BebidasContext)
   return (
-    <div>Bebidas</div>
-  )
+      bebidas.map((bebida) => (
+      <Col md={6} lg={3}>
+      <Card className="mb-5">
+        <Card.Title className="text-center p-2">
+          {bebida.strDrink}
+        </Card.Title>
+        <Card.Img variant="top" src={bebida.strDrinkThumb} />
+        <Card.Body>
+          <Button variant="success" className="w-100">
+            Ver mas
+          </Button>
+        </Card.Body>
+      </Card>
+    </Col>
+      ))
+    
+  );
 }
 
-export default Bebidas
+export { Bebidas };
--- src/components/ListadoBebidas.jsx
+++ src/components/ListadoBebidas.jsx
@@ -3,10 +3,11 @@ import { Card, Row, Spinner, Button, Col } from "react-bootstrap";
 
 // importamos bebidas contexto
 import { BebidasContext } from "../context/BebidasProvider";
+import { Bebidas } from "./Bebidas";
 
 const ListadoBebidas = () => {
   //accediendo al state de bebidas y cargando del context de bebidas
-  const { bebidas, cargando } = useContext(BebidasContext);
+  const { cargando } = useContext(BebidasContext);
 
   return (
     <Row className="justify-content-center mt-5">
@@ -18,26 +19,11 @@ const ListadoBebidas = () => {
           </Spinner>
         )
       }
-
       {
-        //Tarea hacer un componente con todas las cards de bebidas
-        //iterando sobre las bebidas
-        bebidas.map((bebida) => (
-          <Col md={6} lg={3}>
-            <Card className="mb-5">
-              <Card.Title className="text-center p-2">
-                {bebida.strDrink}
-              </Card.Title>
-              <Card.Img variant="top" src={bebida.strDrinkThumb} />
-              <Card.Body>
-                <Button variant="success" className="w-100">
-                  Ver mas
-                </Button>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))
+        <Bebidas />
       }
+        
+      
     </Row>
   );
 };
