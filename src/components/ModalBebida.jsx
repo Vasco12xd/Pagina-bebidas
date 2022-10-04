40d7b276b257ef9a27f92faed0bc1fbfa4a4461e
Esteban Vasco <evasco.27@hotmail.com>
2022-10-04 14:56:15 -0500

    Creación de modal con ingredientes

--- src/components/ModalBebida.jsx
+++ src/components/ModalBebida.jsx
@@ -6,9 +6,18 @@ import {Modal, Image} from 'react-bootstrap';
 import { BebidasContext } from "../context/BebidasProvider";
 
 function ModalBebida() {
-  const [show, setShow] = useState(false);
-
  const { modal, handleClickModal, receta, cargandoModal } = useContext(BebidasContext)
+
+  let ingredientes = [];
+
+  for (let i = 1; i < 16; i++) {
+    if (receta[`strIngredient${i}`]) {
+      ingredientes.push(
+        <li>
+          {receta[`strIngredient${i}`]} - {receta[`strMeasure${i}`]}
+        </li>
+      );
+    }
 
   return (
     !cargandoModal && (
@@ -25,11 +34,16 @@ function ModalBebida() {
           className="img-fluid"
           padding="10px"
           />  
+          <h3 className="mt-4">ingredientes</h3>
+          <ul>
+            {ingredientes}
+          </ul>
+          
         </Modal.Body>
       </Modal>
     </>  
   )
   );
 }
-
-export { ModalBebida };
+}
+export { ModalBebida, };
